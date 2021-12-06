75a7cb71be8347816b6d4cb01323a351bd786552
bombman0188 <95484778+bombman0188@users.noreply.github.com>
2021-12-07 01:07:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
 import time
-import threading
-from bomb_agent import Agent
 
-config_file = "config.toml"
-
-class RepeatTimer(threading.Timer):
-    def run(self):
-        while not self.finished.wait(self.interval):
-            self.function(*self.args, **self.kwargs)
-
-def start():
-    """if agent.check_update() or agent.check_script():
-        agent.stop()
-        agent.do_update()
-        agent.start()
-    """
-    if agent.check_script():
-        agent.stop()
-        agent.start()
-
-if __name__ == "__main__":
-    agent = Agent(config_file)
-    start()
-    # 업데이트 체크 쓰레드 
-    timer = RepeatTimer(30, start)
-    timer.start()
-    timer.join()
+while True:
+    
+    print(1)
+    time.sleep(5)
